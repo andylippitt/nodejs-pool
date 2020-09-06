f9fe7b22799f1cb7fd4274fef1f63372ddf8aa57
Andy Lippitt <andylippitt@gmail.com>
2020-09-05 22:15:34 -0400

    cleanup

--- bake.sh
+++ bake.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-#buildx bake --set *.platform=linux/arm/v7 --set *.platform=linux/amd64 --push
-
-buildx build --platform linux/arm/v7,linux/amd64 -t andylippitt/pool_controller:$1 nodejs-poolController --push
-buildx build --platform linux/arm/v7,linux/amd64 -t andylippitt/pool_web:$1 nodejs-poolController-webClient --push
-buildx build --platform linux/arm/v7,linux/amd64 -t andylippitt/pool_proxy:$1 nodejs-poolController-proxy --push
-buildx build --platform linux/arm/v7,linux/amd64 -t andylippitt/pool_unified:$1 . --push
+buildx bake --set *.platform=linux/arm/v7 --set *.platform=linux/amd64 --push
